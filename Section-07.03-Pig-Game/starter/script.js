74a17efa63b0506b343ade42a4b2787c2c4e3917
mahmoud abdelaziz <mahmoudabdelazizibrahim11@gmail.com>
2025-09-25 01:42:45 +0300

    finish the whole pig game inside the seciton 7

--- Section-07.03-Pig-Game/starter/script.js
+++ Section-07.03-Pig-Game/starter/script.js
@@ -11,16 +11,32 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const player0 = document.querySelector('.player--0');
 const player1 = document.querySelector('.player--1');
+const newGame = document.querySelector('.btn--new');
 
+let scores, currentScore, activePlayer, playing;
 // Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  diceEl.classList.add('hidden');
+  current0.textContent = 0;
+  current1.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+  player0.classList.add('player--active');
+  player1.classList.remove('player--active');
+
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+};
+
+init();
 
 const switchPlayerFun = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -61,7 +77,7 @@ btnHold.addEventListener('click', function () {
 
     // 1. add current score to an active player's score
     // 2. check if playr's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // finish the game
       playing = false;
 
@@ -79,3 +95,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+newGame.addEventListener('click', init);
