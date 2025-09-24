15437769e5b187d4b611ad616aabd94a1c4cd31e
mahmoud abdelaziz <mahmoudabdelazizibrahim11@gmail.com>
2025-09-25 00:07:59 +0300

    run the roll dice button

--- Section-07.03-Pig-Game/starter/script.js
+++ Section-07.03-Pig-Game/starter/script.js
@@ -1,13 +1,22 @@
 'use strict';
 
-let randomNum = Math.trunc(Math.random() * 6) + 1
-console.log(randomNum)
 
 // Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
 
+// Starting Conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden')
+
+btnRoll.addEventListener('click', function(){
+  // Generating a random dic roll
+  const randomNum = Math.trunc(Math.random() * 6) + 1
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${randomNum}.png`;
+})
