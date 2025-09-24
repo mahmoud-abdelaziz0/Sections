60aa1fb6bfd3c1ec854b299e13ab01196995c7c1
mahmoud abdelaziz <itiwork262@gmail.com>
2025-09-24 23:05:21 +0300

    starting of the pig game project in section 7

--- Section-07.03-Pig-Game/starter/script.js
+++ Section-07.03-Pig-Game/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let randomNum = Math.trunc(Math.random() * 6) + 1
+console.log(randomNum)
+
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
